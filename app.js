aaaead0b513325f7802e608f9d8a17ed51206fc6
makhor21 <mkhorshidian72@gmail.com>
2021-06-03 17:11:45 +0430

    final edit

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const app = express();
 const axios = require("axios");
 const schedule = require("node-schedule");
 const moment = require("moment-jalaali");
-moment.locale("fa");
+moment.loadPersian({ usePersianDigits: false, dialect: "persian-modern" });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const sendData = schedule.scheduleJob("*/5 * * * *", async function() {
+const sendData = schedule.scheduleJob("0 0,8,16 * * *", async function() {
     const res = await axios.get(process.env.API_URL);
-    const date = moment(Date.now()).fromNow().format("jYYYY/jM/jD HH:mm");
+    const date = moment()
+        .utcOffset(+270)
+        .format("jYYYY/jM/jD - HH:mm");
     const price = Number(res.data.data.priceUsd).toFixed(2);
     const data = {
         chat_id: "1261111418",
-        text: `${date} - ${price} $`,
+        text: `${date}
+        
+        ${price} $`,
     };
     await axios.post(`${process.env.TELEGRAM_API}/sendMessage`, data);
 });
