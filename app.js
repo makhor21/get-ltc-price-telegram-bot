6d55d5767646d82cd9a7d9f0b44699d440e62cc6
makhor21 <mkhorshidian72@gmail.com>
2021-06-03 23:32:43 +0430

    changing again2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ moment.loadPersian({ usePersianDigits: false, dialect: "persian-modern" });
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const sendData = schedule.scheduleJob("0 16 * * *", async function() {
+const sendData = schedule.scheduleJob("0 21 * * *", async function() {
     const res = await axios.get(process.env.API_URL);
     const date = moment()
         .utcOffset(+270)
