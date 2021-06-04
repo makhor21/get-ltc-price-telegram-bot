63432efd70e609d6a4ab965ec6cbea636a2148a0
makhor21 <mkhorshidian72@gmail.com>
2021-06-04 10:55:18 +0430

    some important changes

--- app.js
+++ app.js
@@ -5,27 +5,44 @@ const axios = require("axios");
 const schedule = require("node-schedule");
 const moment = require("moment-jalaali");
 moment.loadPersian({ usePersianDigits: false, dialect: "persian-modern" });
+const { Telegraf } = require("telegraf");
+
+const bot = new Telegraf(process.env.TOKEN);
+
+bot.command("price", async(ctx) => {
+    const res = await axios.get(process.env.API_URL);
+    const price = Number(res.data.data.priceUsd).toFixed(2);
+    const date = moment()
+        .utcOffset(+270)
+        .format("jYYYY/jM/jD - HH:mm");
+    const data = `<b>${date}</b>
+        
+    ${price} <i>$</i>`;
+    ctx.replyWithHTML(data);
+});
+
+bot.launch();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const sendData = schedule.scheduleJob("0 21 * * *", async function() {
-    const res = await axios.get(process.env.API_URL);
-    const date = moment()
-        .utcOffset(+270)
-        .format("jYYYY/jM/jD - HH:mm");
-    const price = Number(res.data.data.priceUsd).toFixed(2);
-    const data = {
-        chat_id: "1261111418",
-        text: `${date}
-        
-        ${price} $`,
-    };
-    await axios.post(`${process.env.TELEGRAM_API}/sendMessage`, data);
-});
+// const sendData = schedule.scheduleJob("0 21 * * *", async function() {
+//     const res = await axios.get(process.env.API_URL);
+//     const date = moment()
+//         .utcOffset(+270)
+//         .format("jYYYY/jM/jD - HH:mm");
+//     const price = Number(res.data.data.priceUsd).toFixed(2);
+//     const data = {
+//         chat_id: "1261111418",
+//         text: `${date}
+
+//         ${price} $`,
+//     };
+//     await axios.post(`${process.env.TELEGRAM_API}/sendMessage`, data);
+// });
 
 app.get("/", (req, res) => {
-    res.send(`there's nothing to show :D`);
+    res.send(`<p>there's nothing to show :D</p>`);
 });
 
 const port = process.env.PORT || 3000;
